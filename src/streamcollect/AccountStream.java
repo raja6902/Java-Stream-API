7a5d0b8240d0673ceb0d7439e27781125c86018a
Raja Khan <rajashahidullah@hotmail.com>
2020-05-17 20:26:08 +0200

    adding sum function

--- src/streamcollect/AccountStream.java
+++ src/streamcollect/AccountStream.java
@@ -8,23 +8,29 @@ public class AccountStream {
 		BankAccount[] actsArray = {new BankAccount(1,"current", 100), new BankAccount(2,"current", 200),
 				                   new BankAccount(3, "SAVING", 300), new BankAccount(4,"current", 400)};
 		
-		//  find a account balance  greater than 150 in current account and increase a balance by 100.
+		//  find a account balance  greater than 150 in current account and increase a balance by 100 find sum.
+		double sum =0;
 		for (BankAccount act: actsArray) {
 			if(act.getAccountBalance()>150) {
 				if(act.getAccountType().equals("current")) {
 					act.increasebalance(100);
 					System.out.println("Acct number is  " + act.getAccountNumber()+ "  Acct balance is  "  + act.getAccountBalance());
+					sum +=act.getAccountBalance();
 				}
 			}
 		}
+		System.out.println("Sum is  " + sum);
 		System.out.println("________________________________________________________________________");
 		
 		Stream<BankAccount> acctsStream = Stream.of(new BankAccount(1,"current", 100), new BankAccount(2,"current", 200),
 				                                   new BankAccount(3, "SAVING", 300), new BankAccount(4,"current", 400));
-		acctsStream.filter(e ->e.getAccountBalance()>150)
+		double result =acctsStream.filter(e ->e.getAccountBalance()>150)
 		           .filter(e->e.getAccountType().equals("current"))
 		           .peek(e->e.increasebalance(100))
-		           .forEach(e->System.out.println("Acct number is  " + e.getAccountNumber()+ "  Acct balance is  "  + e.getAccountBalance()));
+		           .peek(e->System.out.println("Acct number is  " + e.getAccountNumber()+ "  Acct balance is  "  + e.getAccountBalance()))
+		           .mapToDouble(e->e.getAccountBalance()).sum();
+		
+		System.out.println("Sum is  "+ result);
 				
 		}
 	}
